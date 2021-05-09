05b77d8e7ee15a0acdb2e021182bfe1d1e0619d5
EZ <ezgoesit247@gmail.com>
2021-05-09 15:04:57 -0500

    added User & microposts DAO

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,14 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_05_09_192258) do
+ActiveRecord::Schema.define(version: 2021_05_09_194702) do
+
+  create_table "microposts", force: :cascade do |t|
+    t.text "content"
+    t.integer "user_id"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+  end
 
   create_table "users", force: :cascade do |t|
     t.string "name"
